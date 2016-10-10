36f47e951bd785b35a78835386ebeb17b56ad4d5
Jacob Calkins <jpcalk@gmail.com>
2016-10-09 23:31:30 -0500

    Added an HTML info overlay and finished all gameplay.

--- Scenes/sprint2a.js
+++ Scenes/sprint2a.js
@@ -10,36 +10,61 @@ var playerBox;
 var animFrame = 1;
 var JUMPING = false;
 var PLAYING = true;
+var STRAFFING = false;
 var direction = 1;
 var jumpLength = 1.5;
 var increment = 5;
 var totalDistance = jumpLength / increment;
 
+var lives = 3;
+var READOUT = document.createElement('div');
+
+var lifeDiv = document.createElement("h1");
+lifeDiv.style['float'] = 'right';
+
+var infoDiv = document.createElement("h1");
+infoDiv.style['float'] = 'left';
+
 function scriptOnLoad(currentScene){
     player = currentScene.getObjectByName("Player");
+
     ball = currentScene.getObjectByName("ball");
     donut = currentScene.getObjectByName("donut");
     cylinder = currentScene.getObjectByName("roller1");
+
     var grassMap = currentScene.getObjectByName("ground").material.map;
     grassMap.wrapS = THREE.RepeatWrapping;
     grassMap.wrapT = THREE.RepeatWrapping;
     grassMap.repeat.x = 5;
     grassMap.repeat.y = 3;
 
-    var roadMap1 = currentScene.getObjectByName("road1").material.map;
-    roadMap1.wrapS = THREE.RepeatWrapping;
-    roadMap1.wrapT = THREE.RepeatWrapping;
-    roadMap1.repeat.x = 10;
+    var roadMap;
+    for(var i=1; i <= 3; i++){
+        roadMap = currentScene.getObjectByName("road" + i).material.map;
+        roadMap.wrapS = THREE.RepeatWrapping;
+        roadMap.wrapT = THREE.RepeatWrapping;
+        roadMap.repeat.x = 10;
+    }
 
-    var roadMap2 = currentScene.getObjectByName("road2").material.map;
-    roadMap2.wrapS = THREE.RepeatWrapping;
-    roadMap2.wrapT = THREE.RepeatWrapping;
-    roadMap2.repeat.x = 10;
+    // Create overlay as div element and set its style
+//
+    READOUT.style["position"] = "absolute";      // fixed on screen
+    READOUT.style["width"] = "100%";             // full width of screen
+    READOUT.style["text-align"] = "center";      // text alignment
+    READOUT.style["bottom"] = "90%";             // bottom margin
+    READOUT.style["color"] = "#ff0";             // text color
 
-    var roadMap3 = currentScene.getObjectByName("road3").material.map;
-    roadMap3.wrapS = THREE.RepeatWrapping;
-    roadMap3.wrapT = THREE.RepeatWrapping;
-    roadMap3.repeat.x = 10;
+// Prepend to body so it shows up on top
+//
+    document.body.insertBefore(READOUT, document.body.firstChild);
+
+    var livesCount = document.createTextNode("Lives: " + lives);
+    lifeDiv.appendChild(livesCount);
+    READOUT.appendChild(lifeDiv);
+
+    var info = document.createTextNode(""); //placeholder text
+    infoDiv.appendChild(info);
+    READOUT.appendChild(infoDiv);
 
     copyGeometry(ball, 6);
     copyGeometry(donut, 6);
@@ -55,13 +80,36 @@ function copyGeometry(geometry, copies){
     }
 }
 function keyUpEvent(keyVal) {
-    if(!JUMPING) {
-        if (keyVal == 38) {
-            JUMPING = true;
-            direction = -1;
-        } else if (keyVal == 40) {
-            JUMPING = true;
-            direction = 1;
+    if(player != null) {
+        if (!JUMPING && !STRAFFING) {
+            if (keyVal == 38) {
+                JUMPING = true;
+                player.scale.y = 2;
+                direction = -1;
+            } else if (keyVal == 40) {
+                JUMPING = true;
+                player.scale.y = 2;
+                direction = 1;
+            }
+        }
+        if (!STRAFFING && !JUMPING) {
+            if (keyVal == 37) {
+                STRAFFING = true;
+                player.scale.y = 2;
+                direction = -1;
+            } else if (keyVal == 39) {
+                STRAFFING = true;
+                player.scale.y = 2;
+                direction = 1;
+            }
+        }
+        if(!PLAYING && lives >= 0){
+            if(keyVal == 82){
+                PLAYING = true;
+                player.position.z = 4.5;
+                lifeDiv.textContent = "Lives: " + lives;
+                infoDiv.textContent = "";
+            }
         }
     }
 }
@@ -79,28 +127,50 @@ function jumpScript(sceneNode){
                     var temp = new THREE.Box3().setFromObject(currentScene.children[i]);
                     if (!isNaN(temp.max.x) && playerBox.intersectsBox(temp)) {
                         PLAYING = false;
+                        lives --;
+                        if(lives < 0){
+                            infoDiv.textContent = "You Lose!!!";
+                            // var loserDiv = document.createElement("h1");
+                            // var loser = document.createTextNode("You Lose!!!");
+                            // loserDiv.appendChild(loser);
+                            // READOUT.appendChild(loserDiv);
+                        }else{
+                            infoDiv.textContent = "Press \'r\' to try again!";
+                            // var restart = document.createTextNode("Press \'r\' to try again!");
+                            // restartDiv.style['float'] = "left";
+                            // restartDiv.appendChild(restart);
+                            // READOUT.appendChild(restartDiv);
+                        }
                     }
                 }
             }
+            if (JUMPING && animFrame <= increment) {
+                var rads = (animFrame / increment) * Math.PI;
+                sceneNode.position.y = Math.sin(rads) + 0.5;
+                sceneNode.position.z += (direction * totalDistance);
+                animFrame++;
+                var mat = new THREE.Matrix4();
+                mat.set(
+                    1, 0, 0, 0,
+                    0, animFrame, 0, 0,
+                    0, 0, 1, 0,
+                    0, 0, 0, 1
+                );
+            }else if (STRAFFING && animFrame <= increment) {
+                var rads = (animFrame / increment) * Math.PI;
+                sceneNode.position.y = Math.sin(rads) + 0.5;
+                sceneNode.position.x += (direction * totalDistance);
+                animFrame++;
+            }else if(animFrame > increment){
+                animFrame = 1;
+                JUMPING = false;
+                player.scale.y = 0.5;
+                STRAFFING = false;
+            }else{
+                player.scale.y = 1;
+            }
         }
-        if (JUMPING && animFrame <= increment) {
-            var rads = (animFrame / increment) * Math.PI;
-            sceneNode.position.y = Math.sin(rads) + 0.5;
-            sceneNode.position.z += (direction * totalDistance);
-            //alert(player.position.z);
-            animFrame++;
-             var mat = new THREE.Matrix4();
-             mat.set(
-                1, 0, 0, 0,
-                0, 1.5, 0, 0,
-                0, 0, 1, 0,
-                0, 0, 0, 1
-            );
-            //player.matrix.applyMatrix(mat);
-        } else {
-            animFrame = 1;
-            JUMPING = false;
-        }
+        checkForWin();
     }
 }
 
@@ -138,4 +208,11 @@ function rollScript(sceneNode){
 }
 function degree2rad(degree){
     return degree*(Math.PI/180);
+}
+function checkForWin(){
+    if(player != null && player.position.z < -4.48 && player.scale.y == 1){
+        PLAYING = false;
+        infoDiv.textContent = "Congratulations, you win!!!";
+
+    }
 }
